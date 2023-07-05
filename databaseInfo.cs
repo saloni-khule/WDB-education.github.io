f8f9a2afa0255f5d33d41389cfab8efedc9c7dcb
saloni-khule <91302532+saloni-khule@users.noreply.github.com>
2023-07-05 12:40:19 +0530

    Add files via upload

---
+++ databaseInfo.cs
@@ -0,0 +1,53 @@
+﻿
+//using trial.Models;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+////import Microsoft.EntityFrameworkCore.namespace
+
+//namespace trial
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            var builder = WebApplication.CreateBuilder(args);
+
+//            builder.Services.AddRazorPages();
+
+//            // Add a database provider (import the Microsoft.EntityFrameworkCore namespace!)
+//            builder.Services.AddDbContext<InfoContext>(options =>
+//                options.UseSqlServer("Server=Localinstance3306;Database=sal;Trusted_Connection=True;"));
+
+//            var app = builder.Build();
+
+//            // ...
+//            //...
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+
+
+////"Server=(localdb)\\mssqllocaldb;Database=SchoolDB;Trusted_Connection=True;")
+
+////$host = "localhost";
+////$port = 3306;
+////$socket = "";
+////$user = "root";
+////$password = "";
+////$dbname = "siriusx";
+
+////$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+
+////    or die('Could not connect to the database server' . mysqli_connect_error());
+
+//////$con->close();
+/////
+
+/////mysql connector download
